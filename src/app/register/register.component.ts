4dbd300eb07c9d1f79933b143387d3862f5372c1
Seyed Mostafa Meshkati <meshkat76@gmail.com>
2018-12-29 14:12:05 +0330

    Season 6 Episode 8

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,8 +8,13 @@ import { NgForm, FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
   gender = "male";
-  
   registerForm: FormGroup;
+
+  invalidPassword = [
+    "1234",
+    "abcd",
+    "qwerty"
+  ]
 
   constructor() { }
 
@@ -20,13 +25,23 @@ export class RegisterComponent implements OnInit {
         gender: new FormControl('female')
       }),
       email: new FormControl(null, [Validators.required, Validators.email]),
-      password: new FormControl(null, Validators.required)
+      password: new FormControl(null, [Validators.required, this.lowPasswords.bind(this)])
     });
   }
 
   onSubmit() {
     console.log("Submitted!");
     console.log(this.registerForm);  
+  }
+
+  lowPasswords(control: FormControl): {[lowLevelPassword: string]: boolean} {
+    if (this.invalidPassword.indexOf(control.value) != -1) {
+      return {
+        lowLevelPassword: true
+      }
+    }
+
+    return null;
   }
 
 }
